40d7b3824af9057c485e35c98df6be7ed0b0f5c7
RuthKibos <kibosruth@gmail.com>
2024-06-09 23:17:14 +0300

    Coded Along

--- lib/Stack.py
+++ lib/Stack.py
@@ -1,25 +1,39 @@
 class Stack:
 
-    def __init__(self, items = [], limit = 100):
-        pass
+    def __init__(self, items=[], limit=100):
+        if items is None:
+            items = []
+        self.items = items
+        self.limit = limit
 
     def isEmpty(self):
-        pass
+        return len(self.items) == 0
 
     def push(self, item):
-        pass
+        if len(self.items) < self.limit:
+            self.items.append(item)
+        else:
+            OverflowError: Stack is full
 
     def pop(self):
-        pass
+        if not self.isEmpty():
+            return self.items.pop()
+        return None
 
     def peek(self):
-        pass
-    
+        if not self.isEmpty():
+            return self.items[-1]
+        return None
+
     def size(self):
-        pass
+        return len(self.items)
 
     def full(self):
-        pass
+        return len(self.items) == self.limit
 
     def search(self, target):
-        pass
+        if target in self.items:
+            return len(self.items) - self.items.index(target) - 1
+        return -1
+
+    
